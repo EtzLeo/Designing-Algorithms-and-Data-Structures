7c5378709e1548160b04600a1437eb2ad3eefe71
Kseniia <kbasloviak@gmail.com>
2020-03-23 11:18:47 +0300

    add files

---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Calibri Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(258, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Список смежности";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Calibri Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(522, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Матрица смежности вершин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Calibri Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(833, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Матрица смежности рёбер";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Calibri Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1157, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Матрица инцидентности\r\n";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(211, 312);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(265, 291);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "0:1,2\n1:0\n2:1";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(511, 312);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(265, 291);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox3.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox3.Location = new System.Drawing.Point(818, 312);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(10);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(265, 291);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox4.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox4.Location = new System.Drawing.Point(1130, 312);
+            this.richTextBox4.Margin = new System.Windows.Forms.Padding(10);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(265, 291);
+            this.richTextBox4.TabIndex = 7;
+            this.richTextBox4.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(340, 658);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(355, 89);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Заполнить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox5.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox5.Location = new System.Drawing.Point(1423, 312);
+            this.richTextBox5.Margin = new System.Windows.Forms.Padding(10);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(265, 291);
+            this.richTextBox5.TabIndex = 9;
+            this.richTextBox5.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Calibri Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1502, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Список рёбер";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(1225, 658);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 89);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[,] vertexesMatrix;
+        public int[,] edgesMatrix;
+        public int[,] incidenceMatrix;
+        public string str;
+        public int vertCount = 0;
+        public int edgesCount = 0;
+
+        interface testInterface
+        {
+            int TransmitterVertex { get; set; }
+            int TransmitterDirection { get; set; }
+            int ReceiverVertex { get; set; }
+            int ReceiverDirection { get; set; }
+            void EdgeSetUp(int TransmitterValue, int ReceiverValue, bool isOriented);
+        }
+        public struct testEdge:testInterface
+        {
+            public int transmitterVertex;
+            public int transmitterDirection;
+            public int receiverVertex;
+            public int receiverDirection;
+            public testEdge(int TransmitterValue, int ReceiverValue, bool isOriented)
+            {
+                this.transmitterVertex = TransmitterValue;
+                this.receiverVertex = ReceiverValue;
+                this.transmitterDirection = 1;
+                this.receiverDirection = 1;
+                if (isOriented)
+                {
+                    this.receiverDirection = -1;
+                }
+            }
+            public int TransmitterVertex { get => this.transmitterVertex; set => this.transmitterVertex = value; }
+            public int TransmitterDirection { get => this.transmitterDirection; set => this.transmitterDirection = value; }
+            public int ReceiverVertex { get => this.receiverVertex; set => this.receiverVertex = value; }
+            public int ReceiverDirection { get => this.receiverDirection;  set => this.receiverDirection = value;  }
+            public void EdgeSetUp(int transmitterValue, int receiverValue, bool isOriented)
+            {
+                this.transmitterVertex = transmitterValue;
+                this.receiverVertex = receiverValue;
+                this.transmitterDirection = 1;
+                this.receiverDirection = 1;
+                if (isOriented)
+                {
+                    this.receiverDirection = -1;
+                }
+            }
+
+        }
+        List<testInterface> test = new List <testInterface>();
+        public void InitMatrix(int[,] matrix, int str, int col)
+        {
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+        public void DeleteEdges()//поиск неориентированного ребра
+        {
+            for (int i = 0; i < test.Count; i++)
+            {
+                for (int j = i + 1; j < test.Count; j++) 
+                {
+                    if (test[i].TransmitterVertex == test[j].ReceiverVertex && test[j].TransmitterVertex == test[i].ReceiverVertex)
+                    {
+                        test.RemoveAt(j);
+                        test[i].ReceiverDirection = 1;
+                    }
+                }
+            }
+        }
+        public void SetMatrixSize()//из списка смежности создание списка ребер
+        {
+            str = richTextBox1.Text;
+            int edgeNum = 0;
+            vertCount++;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i] == '\n')
+                {
+                    str = str.Remove(i, 1);
+                    vertCount++;
+                }
+                if (str[i] == ':' && str[i + 1] != '\n') 
+                {
+                    test.Add(new testEdge((int)Char.GetNumericValue(str[i - 1]), (int)Char.GetNumericValue(str[i + 1]), true));
+                    edgeNum = (int)Char.GetNumericValue(str[i - 1]);
+                }
+                if (str[i] == ',')
+                {
+                    test.Add(new testEdge(edgeNum, (int)Char.GetNumericValue(str[i + 1]), true));
+                }
+            }
+            DeleteEdges();
+        }
+        public void FillVertexesMatrix()// заполнение матрицы вершин из списка смежности
+        {
+            vertexesMatrix = new int[vertCount, vertCount];
+            InitMatrix(vertexesMatrix, vertCount, vertCount);
+            int k = 0;
+            for (int i = 0; i < vertCount; i++)
+            {
+                for (int j = i+1; j < vertCount; j++)
+                {
+                    if (k < test.Count)
+                    {
+
+                        vertexesMatrix[test[k].TransmitterVertex, test[k].ReceiverVertex] = 1;
+
+                        if (test[k].ReceiverDirection == 1)
+                        {
+                            vertexesMatrix[test[k].ReceiverVertex, test[k].TransmitterVertex] = 1;
+                        }
+                        k++;
+                    }
+                }
+
+                for (int l = 0; l < vertCount; l++)
+                {
+                    richTextBox2.Text += Convert.ToString(vertexesMatrix[i, l]) + ' ';
+                }
+                richTextBox2.Text += "\n";
+            }
+        }
+        public void SetVertexesMatrix()//заполнение матрицы вершин из текстбокса
+        {
+            string str = richTextBox2.Text;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Equals('\n'))
+                {
+                    vertCount++;
+                }
+            }
+            vertCount++;
+            vertexesMatrix = new int[vertCount, vertCount];
+            InitMatrix(vertexesMatrix, vertCount, vertCount);
+            int k = 0, j = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!str[i].Equals('\n'))
+                {
+                    vertexesMatrix[k, j] = (int)Char.GetNumericValue(str[i]);
+                    j++;
+                }
+                else
+                {
+                    k++;
+                    j = 0;
+                }
+            }
+        }
+        public void FillEdgeStruct()//заполнение списка ребер из матрицы вершин
+        {
+            for (int i = 0; i < vertCount; i++)
+            {
+                for (int j = 0; j < vertCount; j++)
+                {
+                    if(vertexesMatrix[i,j] == 1)
+                    {
+                        test.Add( new testEdge(i, j, true));
+                    }
+                }
+            }
+            DeleteEdges();
+        }
+        public void FillEdgesMatrix()//заполнение матрицы ребер из списка ребер
+        {
+            edgesMatrix = new int[test.Count, test.Count];
+            InitMatrix(edgesMatrix, test.Count, test.Count);
+
+            for (int i = 0; i < test.Count; i++)
+            {
+                for (int k = 0; k < test.Count; k++)
+                {
+                    if (i != k) 
+                    {
+                        if (test[i].ReceiverDirection == -1 && test[k].ReceiverDirection == -1)
+                        {
+                            if (test[i].TransmitterVertex == test[k].TransmitterVertex ||
+                                test[i].ReceiverVertex == test[k].ReceiverVertex)
+                            {
+                                edgesMatrix[i, k] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if (test[i].TransmitterVertex == test[k].TransmitterVertex ||
+                                test[i].TransmitterVertex == test[k].ReceiverVertex ||
+                                test[i].ReceiverVertex == test[k].TransmitterVertex ||
+                                test[i].ReceiverVertex == test[k].ReceiverVertex)
+                            {
+                                edgesMatrix[i, k] = 1;
+                            }
+                        }
+                    }
+                }
+
+                for (int j = 0; j < test.Count; j++)
+                {
+                    richTextBox3.Text += Convert.ToString(edgesMatrix[i, j]) + ' ';
+                }
+                if (i + 1 != test.Count)
+                    richTextBox3.Text += "\n";
+            }
+        }
+        public void SetEdgesMatrix()//заполнение матрицы из текстбокса// не понятно, какие ребра смежны, матрица бесполезна
+        {
+            string str = richTextBox3.Text;
+            for (int i = 0; i < str.Length && !str[i].Equals('\n'); i++)
+            { 
+                edgesCount++;
+            }
+            edgesMatrix = new int[edgesCount, edgesCount];
+            int k = 0;
+            for (int i = 0; i < edgesCount; i++)
+            {
+                for (int j = 0; j < edgesCount; j++)
+                {
+                    if (str[k].Equals(Convert.ToChar(10)))
+                    {
+                        str = str.Remove(k, 1);
+                    }
+                    edgesMatrix[i, j] = (int)Char.GetNumericValue(str[k]);
+                    k++;
+                }
+
+            }
+        }
+        public void FillIncedenceMatrix()//заполнение матрицы инцидентности из списка ребер
+        {
+            incidenceMatrix = new int[vertCount, test.Count];
+            InitMatrix(incidenceMatrix, vertCount, test.Count);
+
+            for (int j = 0; j < test.Count; j++)
+            {
+                    incidenceMatrix[test[j].TransmitterVertex, j] = test[j].TransmitterDirection;
+                    incidenceMatrix[test[j].ReceiverVertex, j] = test[j].ReceiverDirection;
+            }
+
+            for (int i = 0; i < vertCount; i++)
+            {
+                for (int k = 0; k < test.Count; k++)
+                {
+                    richTextBox4.Text += Convert.ToString(incidenceMatrix[i, k]) + ' ';
+                }
+                richTextBox4.Text += "\n";
+            }
+
+        }
+        public void SetIncidenceMatrix()//заполнение матрицы инц из текстбокса
+        {
+            string str = richTextBox4.Text;
+            int i = 0, k = 0;
+            vertCount = 1;
+            while (!str[i].Equals('\n'))
+            {
+                if (!str[i].Equals('-'))
+                {
+                    edgesCount++;
+                }
+                i++;
+            }
+            for (i=0; i < str.Length; i++,k++)
+            {
+                if(str[k + 1].Equals('\n'))
+                {
+                    vertCount++;
+                    str = str.Remove(k+1, 1);
+                    k--;
+                } 
+            }
+
+            incidenceMatrix = new int[vertCount, edgesCount];
+
+            k = 0;
+            for (i = 0; i < vertCount; i++)
+            {
+                for (int j = 0; j < edgesCount; j++)
+                {
+                    if (k < str.Length)
+                    {
+                        if (str[k] != '-')
+                        {
+                            incidenceMatrix[i, j] = (int)Char.GetNumericValue(str[k++]);
+                        }
+                        else
+                        {
+                            k++;
+                            incidenceMatrix[i, j] = -(int)Char.GetNumericValue(str[k++]);
+                        }
+                    }
+                }
+
+            }
+        }
+        public void FillAdjList()//заполнение списка смежности (и списка ребер) из матрицы инцедентности
+        {
+            string[] strMas = new string[vertCount];
+
+            for (int i = 0; i < vertCount; i++)
+            {
+                for (int j = 0; j < edgesCount; j++)
+                {
+                    if (incidenceMatrix[i, j] == 1)
+                    {
+                        for (int k = 0; k < vertCount; k++)
+                        {
+                            if (k != i)
+                            {
+                                if (incidenceMatrix[k, j] == 1)
+                                {
+                                    test.Add(new testEdge(i, k, false));
+                                }
+                                if (incidenceMatrix[k, j] == -1)
+                                {
+                                    test.Add(new testEdge(i, k, true));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            DeleteEdges();
+            for (int i = 0; i < test.Count; i++)
+            {
+                strMas[test[i].TransmitterVertex] += Convert.ToString(test[i].ReceiverVertex)+',';
+                if (test[i].ReceiverDirection == 1)
+                {
+                    strMas[test[i].ReceiverVertex] += Convert.ToString(test[i].TransmitterVertex)+',';
+                }
+            }
+            for (int i = 0; i < vertCount; i++)
+            {
+                strMas[i] = strMas[i].Remove(strMas[i].Length-1);
+                richTextBox1.Text += Convert.ToString(i) + ':' + strMas[i] + '\n';
+            }
+        }
+        public void EdgesList()//вывод списка ребер в текстбокс
+        {
+            for (int i = 0; i < test.Count; i++)
+            {
+                richTextBox5.Text += i + ":" + test[i].TransmitterVertex + test[i].ReceiverVertex + "  " + test[i].ReceiverDirection + "\r\n";
+            }
+        }     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                richTextBox2.Clear();
+                richTextBox3.Clear();
+                richTextBox4.Clear();
+                richTextBox5.Clear();
+
+                SetMatrixSize();
+                FillVertexesMatrix();
+                FillEdgesMatrix();
+                FillIncedenceMatrix();
+                EdgesList();
+            }
+            else
+            {
+                if (richTextBox2.Text != "")
+                {
+                    richTextBox3.Clear();
+                    richTextBox4.Clear();
+                    richTextBox5.Clear();
+
+                    SetVertexesMatrix();
+                    FillEdgeStruct();
+                    FillEdgesMatrix();
+                    FillIncedenceMatrix();
+                    FillAdjList();
+                    EdgesList();
+                }
+                else {
+                    if (richTextBox3.Text != "")
+                    {
+                        richTextBox4.Clear();
+                        richTextBox5.Clear();
+
+                        SetEdgesMatrix();
+                        FillIncedenceMatrix();
+                        FillAdjList();
+                        SetMatrixSize();
+                        FillVertexesMatrix();
+                        EdgesList();
+                    }
+                    else
+                    {
+                        if (richTextBox4.Text != "")
+                        {
+                            richTextBox5.Clear();
+
+                            SetIncidenceMatrix();
+                            FillAdjList();
+                            EdgesList();
+                            FillVertexesMatrix();
+                            FillEdgesMatrix();
+                        }
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            richTextBox5.Clear();
+        }
+    }
+}
