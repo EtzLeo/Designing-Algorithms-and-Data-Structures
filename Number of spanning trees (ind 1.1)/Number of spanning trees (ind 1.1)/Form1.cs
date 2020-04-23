fec04b1fcb7dc238bcf4125ac8955b4699192050
Kseniia <kbasloviak@gmail.com>
2020-04-23 19:34:13 +0300

    Добавление элементов на форму, создание скелета программы

--- Number of spanning trees (ind 1.1)/Number of spanning trees (ind 1.1)/Form1.cs
+++ Number of spanning trees (ind 1.1)/Number of spanning trees (ind 1.1)/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Number_of_spanning_trees__ind_1._1_
 {
@@ -15,6 +16,47 @@ namespace Number_of_spanning_trees__ind_1._1_
         public Form1()
         {
             InitializeComponent();
+        }
+        public int[,] vertexMatrix;
+        public int kirchhoffMatrix;
+        public int matrixSize;
+
+        public void CreateKirchhoffMatrix() { }
+        public void TransformMatrix() { }
+        public int FindDeterminant() { return 0; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int t = 0;
+            if (richTextBox1.Text.Equals(""))
+            {
+                StreamReader ifstream = new StreamReader("matrix.txt", System.Text.Encoding.Default);
+                richTextBox1.Text += ifstream.ReadToEnd();
+                ifstream.Close();
+            }
+
+            while (!richTextBox1.Text[t++].Equals('\n'))
+            {
+                matrixSize++;
+            }
+            vertexMatrix = new int[matrixSize, matrixSize];
+            t = 0;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    vertexMatrix[i, j] = (int)Char.GetNumericValue(richTextBox1.Text[t++]);
+                }
+                t++;
+            }
+
+            CreateKirchhoffMatrix();
+            int det = FindDeterminant();
+            StreamWriter ffstream = new StreamWriter("result.txt", false, System.Text.Encoding.Default);
+            ffstream.Write(det);
+            ffstream.Close();
+            label3.Text = Convert.ToString(det);
         }
     }
 }
